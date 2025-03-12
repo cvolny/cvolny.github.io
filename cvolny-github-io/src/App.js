1965f5b50bf845779175e940d20bdfaa6f0be685
Frau Cupcake Volny <cvolny@gmail.com>
2025-03-12 18:58:28 +0100

    adding workaround from https://stackoverflow.com/a/47420338

--- cvolny-github-io/src/App.js
+++ cvolny-github-io/src/App.js
@@ -18,7 +18,7 @@ import NoPage from "./pages/NoPage";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={`/${process.env.PUBLIC_URL}`}>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
